7959d20cc21d9d94b6fe61a1bceab000e12a8b5c
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-08-13 20:02:44 -0700

    Implemented binary search

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -2,5 +2,21 @@
 #include <vector>
 
 int binarySearch(std::vector<int> list, int val) {
+	int low = 0;
+	int high = list.size() - 1;
+
+	while (low <= high) {
+		int med = low + ((high - low) / 2);
+		if (list[med] < val) {
+			low = med + 1;
+		}
+		else if (list[med] > val) {
+			high = med - 1;
+		}
+		else {
+			return med;
+		}
+	};
+
 	return -1;
 }
--- main.cpp
+++ main.cpp
@@ -2,21 +2,16 @@
 #include <vector>
 #include "printList.h"
 
+// Sorting
+#include "bubbleSort.h"
+
 // Search
 #include "linearSearch.h"
 #include "binarySearch.h"
 
-// Sorting
-#include "bubbleSort.h"
-
 int main()
 {
     std::vector<int> arr = {72, 5, 123, 7, 34, 8, 23, 9, 10, 12, 87, 9, 321, 674, 34, 9};
-
-    // Search
-    // Linear Search
-    std::cout << "Linear Search: " << linearSearch(arr, 12) << std::endl;
-
     
     // Sorting
     std::cout << "Before Sort:" << std::endl;
@@ -27,6 +22,13 @@ int main()
     std::cout << "Bubble Sort:" << std::endl;
     bubbleSort(arr);
     printList(arr);
+
+    // Search
+    // Linear Search
+    std::cout << "Linear Search: " << linearSearch(arr, 12) << std::endl;
+
+    // Binary Search
+    std::cout << "Binary Search: " << binarySearch(arr, 34) << std::endl;
 
     return 0;
 }
