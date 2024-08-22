e6047fcfb03d3477d0a2a79e9bbc606a6d6f503b
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-08-21 21:02:18 -0700

    Started quick sort

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,6 +1,14 @@
 #include "quicksort.h"
 #include <vector>
 
-void quickSort(std::vector<int>& list) {
+static void qsHelper(std::vector<int>& list, int low, int high) {
 
+}
+
+static void partition(std::vector<int>& list, int low, int high) {
+
+}
+
+void quickSort(std::vector<int>& list) {
+	qsHelper(list, 0, list.size() - 1);
 }
