5f1b94f9591bf2adacd69363f613de777f2c3a64
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-08-22 14:28:50 -0700

    Implemented quick sort

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,12 +1,34 @@
 #include "quicksort.h"
 #include <vector>
 
-static void qsHelper(std::vector<int>& list, int low, int high) {
+static int partition(std::vector<int>& list, int low, int high) {
+	int pivot = list[high];
+	int pivotIndex = low - 1;
 
+	for (int i = low; i < high; ++i) {
+		if (list[i] <= pivot) {
+			pivotIndex++;
+			int temp = list[i];
+			list[i] = list[pivotIndex];
+			list[pivotIndex] = temp;
+		}
+	}
+	pivotIndex++;
+	list[high] = list[pivotIndex];
+	list[pivotIndex] = pivot;
+
+	return pivotIndex;
 }
 
-static void partition(std::vector<int>& list, int low, int high) {
+static void qsHelper(std::vector<int>& list, int low, int high) {
+	if (high <= low) {
+		return;
+	}
 
+	int pivotIndex = partition(list, low, high);
+
+	qsHelper(list, low, pivotIndex - 1);
+	qsHelper(list, pivotIndex + 1, high);
 }
 
 void quickSort(std::vector<int>& list) {
